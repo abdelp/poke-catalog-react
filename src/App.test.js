00a72151ebef5333627bbe9ffa86f492d1cde3cf
Abdel <juniorperezpy@gmail.com>
2020-10-09 14:04:18 -0300

    Move App.js and App.css to components folder

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from './app/store';
-import App from './App';
+import App from './components/App/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
