b890efcf56618bb0a0946f84d42f61a2d0bc6b3b
Abdel <juniorperezpy@gmail.com>
2020-10-11 21:05:21 -0300

    Add Navigation component structure with home, categories, services, support and contact links

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,7 +6,11 @@ import * as ROUTES from '../../constants/routes';
 const Navigation = () => (
   <ul>
     <li>
-      <Link to={ROUTES.HOME}>Home</Link>
+      <Link to={ROUTES.HOME}>HOME</Link>
+      <Link to={ROUTES.HOME}>CATEGORIES</Link>
+      <Link to={ROUTES.HOME}>SERVICES</Link>
+      <Link to={ROUTES.HOME}>SUPPORT</Link>
+      <Link to={ROUTES.HOME}>CONTACT</Link>
     </li>
   </ul>
 );
