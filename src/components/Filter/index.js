e960a947ad73a9004663a88bcc342e0d35eb0b5a
Abdel <juniorperezpy@gmail.com>
2020-10-12 13:48:41 -0300

    Refactor Filter component to connect with redux store

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import cx from 'classnames';
+import { VISIBILITY_FILTERS } from '../../constants/visibilityFilters';
+import { connect } from 'react-redux';
+import { setFilter } from '../../redux/actions';
 
-const Filter = () => (
-  <form>
-    <input />
-    <button type="button">Search (just icon)</button>
-  </form>
+const VisibilityFilters = ({ activeFilter, setFilter }) => {
+  const handleOnChangeEvent = event =>
+    setFilter(event.target.value);
+
+  return (
+  <div className='visibility-filters'>
+    <select
+      id='pokemon-types-select'
+      onChange={handleOnChangeEvent}>
+
+    {Object.keys(VISIBILITY_FILTERS).map(filterKey => {
+      const currentFilter = VISIBILITY_FILTERS[filterKey];
+      return (
+        <option
+          key={`visibility-filter-${currentFilter}`}
+          className={cx(
+            'filter',
+            currentFilter === activeFilter && 'filter--active'
+          )}
+        >
+          {currentFilter}
+        </option>
+      );
+    })}
+    </select>
+  </div>
 );
+};
 
-export default Filter;
+const mapStateToProps = state =>
+  ({ activeFilter: state.visibilityFilter });
+
+export default connect(
+  mapStateToProps,
+  { setFilter }
+)(VisibilityFilters);
