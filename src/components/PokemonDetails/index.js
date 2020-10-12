9504ccbd843b4217078d1cd50cabae72879ae85d
Abdel <juniorperezpy@gmail.com>
2020-10-12 16:46:16 -0300

    Add PropTypes for PokemonDetailsPage

--- src/components/PokemonDetails/index.js
+++ src/components/PokemonDetails/index.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getPokemonDetails } from '../../APIs/pokemons';
+import React from 'react';
+import PropTypes from 'prop-types';
 import PokemonTypes from '../PokemonTypes';
 import PokemonAbilities from '../PokemonAbilities';
 import PokemonStats from '../PokemonStats';
@@ -8,40 +7,66 @@ import DataBox from '../DataBox';
 
 const PokemonDetailsPage = props => {
   const { location: { state: { pokemon } } } = props;
-
-  console.log(props);
+  const {
+    name,
+    number,
+    img,
+    stats,
+    height,
+    weight,
+    types,
+    abilities,
+  } = pokemon;
 
   return (
     <>
       <h1 className="pokemon-name">
-        {pokemon.name}
+        {name}
         {' '}
         <span>
           #
-          {pokemon.number}
+          {number}
         </span>
       </h1>
       <div className="details-container">
         <div className="left-side">
           <div className="img-container">
-            <img src={pokemon.img} alt={pokemon.name} />
+            <img src={img} alt={name} />
           </div>
           <div className="stats-container">
-            <PokemonStats stats={pokemon.stats} />
+            <PokemonStats stats={stats} />
           </div>
         </div>
         <div className="right-side">
-          <DataBox height={pokemon.height} weight={pokemon.weight} />
+          <DataBox height={height} weight={weight} />
 
           <h2>Type</h2>
-          <PokemonTypes types={pokemon.types} />
+          <PokemonTypes types={types} />
 
           <h2>Abilities</h2>
-          <PokemonAbilities abilities={pokemon.abilities} />
+          <PokemonAbilities abilities={abilities} />
         </div>
       </div>
     </>
   );
+};
+
+PokemonDetailsPage.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      pokemon: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        img: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+        types: PropTypes.arrayOf(PropTypes.object).isRequired,
+        stats: PropTypes.arrayOf(PropTypes.object).isRequired,
+        abilities: PropTypes.arrayOf(PropTypes.object).isRequired,
+        height: PropTypes.number.isRequired,
+        weight: PropTypes.number.isRequired,
+      }),
+    }),
+  }).isRequired,
 };
 
 export default PokemonDetailsPage;
