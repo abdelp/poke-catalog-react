889bb84d720aea9bf3d57f16c1abeecb47864b5a
Abdel <juniorperezpy@gmail.com>
2020-10-11 21:03:15 -0300

    Add initial routes for App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
+// import logo from './logo.svg';
 import './App.css';
+import Navigation from '../Navigation';
+import HomePage from '../Home';
+import PokemonDetailsPage from '../PokemonDetails';
+
+import { getPokemons } from '../../APIs/pokemons';
 
 function App() {
-  return (<>
-  </>
+  return (
+    <Router>
+      <div>
+        <Navigation />
+        <hr />
+
+        <Switch>
+          <Route exact path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.POKEMON_DETAILS} component={PokemonDetailsPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
