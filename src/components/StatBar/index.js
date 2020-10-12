94ff86930e920ce72390a3bc4c4acf5881cf5f01
Abdel <juniorperezpy@gmail.com>
2020-10-11 21:08:25 -0300

    Add initial StatBar component structure

---
+++ src/components/StatBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import cx from 'classnames';
+
+const StatBar = ({ className }) => {
+  let children = [];
+  for (let i = 0; i < 15; i += 1) {
+    children.push(<li>1</li>);
+  }
+
+  return (
+    <ul>
+      { children }
+    </ul>
+  );
+};
+
+export default StatBar;
