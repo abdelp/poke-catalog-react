dc228e6c5c3c84b93d03c3f84a106f8bbeb32515
Abdel <juniorperezpy@gmail.com>
2020-10-12 16:50:20 -0300

    Remove unnecessary useEffect and getPokemons functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -6,13 +6,10 @@ import {
 } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
-// import logo from './logo.svg';
 import './App.css';
 import Navigation from '../Navigation';
 import HomePage from '../Home';
 import PokemonDetailsPage from '../PokemonDetails';
-
-import { getPokemons } from '../../APIs/pokemons';
 
 function App() {
   return (
