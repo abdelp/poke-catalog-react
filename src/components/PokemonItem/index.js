74400682c38914af8d29a79d5b15b7bda806a696
Abdel <juniorperezpy@gmail.com>
2020-10-12 16:31:28 -0300

    Add PropTypes for PokemonItem

--- src/components/PokemonItem/index.js
+++ src/components/PokemonItem/index.js
@@ -1,29 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import PokemonTypes from '../PokemonTypes';
 
-const PokemonItem = ({ pokemon }) => (
-  <Link to={{
-    pathname: `/pokemon-details/${pokemon.id}`,
-    state: { pokemon },
-  }}
-  >
-    <div className="pokemon-card">
-      <header className="card-header">
-        <img src={pokemon.img} alt={pokemon.name} />
-      </header>
-      <main className="card-body">
-        <h6>
-          #
-          {pokemon.number}
-        </h6>
-        <h2>{pokemon.name}</h2>
-      </main>
-      <footer className="card-footer">
-        <PokemonTypes types={pokemon.types} />
-      </footer>
-    </div>
-  </Link>
-);
+const PokemonItem = ({ pokemon }) => {
+  const {
+    id, img, name, number, types,
+  } = pokemon;
+  return (
+    <Link to={{
+      pathname: `/pokemon-details/${id}`,
+      state: { pokemon },
+    }}
+    >
+      <div className="pokemon-card">
+        <header className="card-header">
+          <img src={img} alt={name} />
+        </header>
+        <main className="card-body">
+          <h6>
+            #
+            {number}
+          </h6>
+          <h2>{name}</h2>
+        </main>
+        <footer className="card-footer">
+          <PokemonTypes types={types} />
+        </footer>
+      </div>
+    </Link>
+  );
+};
+
+PokemonItem.propTypes = {
+  pokemon: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.number.isRequired,
+    types: PropTypes.arrayOf(PropTypes.object).isRequired,
+  }).isRequired,
+};
+
+// PokemonTypes.defaultProps = {
+//   pokemon: {
+//     id: null,
+//     img: null,
+//     name: null,
+//     number: null,
+//     types: null,
+//   },
+// };
 
 export default PokemonItem;
