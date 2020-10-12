a872732c95beed0afe48fa37ce102a53e4904164
Abdel <juniorperezpy@gmail.com>
2020-10-11 21:07:41 -0300

    Add initial PokemonStats component structure

---
+++ src/components/PokemonStats/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import cx from 'classnames';
+import StatBar from '../StatBar';
+
+const PokemonStats = ({ stats }) => (
+  <>
+    <StatBar />
+    <StatBar />
+    <StatBar />
+    <StatBar />
+    <StatBar />
+    <StatBar />
+  </>
+);
+
+export default PokemonStats;
