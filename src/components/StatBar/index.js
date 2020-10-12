dad3ae282054511180821dba22d48fd9f6801820
Abdel <juniorperezpy@gmail.com>
2020-10-12 15:48:39 -0300

    Add prop-types validation and default values to StatBar

--- src/components/StatBar/index.js
+++ src/components/StatBar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import cx from 'classnames';
+import PropTypes from 'prop-types';
 
 const StatBar = ({ className }) => {
   const children = [];
@@ -8,10 +8,18 @@ const StatBar = ({ className }) => {
   }
 
   return (
-    <ul>
+    <ul className={className}>
       { children }
     </ul>
   );
+};
+
+StatBar.propTypes = {
+  className: PropTypes.string,
+};
+
+StatBar.defaultProps = {
+  className: '',
 };
 
 export default StatBar;
