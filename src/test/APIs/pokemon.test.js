d00c982d66005f7ad0d4f0358d993b9e87bf8fe1
Abdel <juniorperezpy@gmail.com>
2020-10-11 21:09:56 -0300

    Add non working api test

--- src/test/APIs/pokemon.test.js
+++ src/test/APIs/pokemon.test.js
@@ -1,19 +1,35 @@
 import axios from 'axios';
-import { getPokemons, API_BASE } from '../../APIs/pokemons';
+import { getPokemon, getPokemons, API_BASE } from '../../APIs/pokemons';
 
 jest.mock('axios');
 
 describe('pokemons API', () => {
   it('fetches successfully pokemons data from the API', async () => {
-    const data = { data: {
-      pokemon_entries: [
-        {entry_number: 1,
-         pokemon_species: { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon-species/1/" }},
-        {entry_number: 2,
-         pokemon_species: {name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon-species/2/" }}
-      ]}};
+    const data = { data: [
+      {
+        "count": 1050,
+        "next": "https://pokeapi.co/api/v2/pokemon?offset=3&limit=3",
+        "previous": null,
+        "results": [
+          {
+            "name": "bulbasaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/1/"
+          },
+          {
+            "name": "ivysaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/2/"
+          },
+          {
+            "name": "venusaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/3/"
+          }
+        ]
+      }
+    ]};
 
     axios.get.mockImplementationOnce(() => Promise.resolve(data));
+    axios.get.mockImplementationOnce(() => Promise.resolve(data));
+
     await expect(getPokemons()).resolves.toEqual(data.data.pokemon_entries);
 
     expect(axios.get).toHaveBeenCalledWith(
