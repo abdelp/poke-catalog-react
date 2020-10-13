2d223b0614c15a746038b7da3092ec0d2b48883c
Abdel <juniorperezpy@gmail.com>
2020-10-13 18:50:15 -0300

    Update structure of navigator to put each Link inside a li tag

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,13 +4,21 @@ import { Link } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
 const Navigation = () => (
-  <ul>
+  <ul className='nav'>
     <li>
-      <Link to={ROUTES.HOME}>HOME</Link>
+      <Link to={ROUTES.HOME}>POKEDEX</Link>
+    </li>
+    <li>
       <Link to={ROUTES.HOME}>CATEGORIES</Link>
-      <Link to={ROUTES.HOME}>SERVICES</Link>
-      <Link to={ROUTES.HOME}>SUPPORT</Link>
-      <Link to={ROUTES.HOME}>CONTACT</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>TOURNAMENTS</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>GYMS</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>RANKING</Link>
     </li>
   </ul>
 );
