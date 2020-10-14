0331c24ad097c83411e1ff5d5dd35136dcf8e66e
Abdel <juniorperezpy@gmail.com>
2020-10-14 10:24:50 -0300

    Remove unnecesarry options for images removed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { sortBy } from 'lodash';
